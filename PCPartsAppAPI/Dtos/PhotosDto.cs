091a27500ea4b4621769c1f83497d9f70a83452c
Tomasz Bąk <tomasz.bak@euvic.pl>
2022-01-07 17:31:56 +0100

    feat: added ui

---
+++ PCPartsAppAPI/Dtos/PhotosDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCPartsAppAPI.Dtos
+{
+    public class PhotosDto
+    {
+        public int AnnouncementId { get; set; }
+        public IFormFile[] ImagePaths { get; set; }
+    }
+}
---
+++ PCPartsAppAPI/Dtos/SearchDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCPartsAppAPI.Dtos
+{
+    public class SearchDto
+    {
+        public string Querry { get; set; }
+        public bool IsSet { get; set; }
+        public string Category { get; set; }
+        public double MaxPrice { get; set; }
+        public double MinPrice { get; set; }
+        public string City { get; set; }
+    }
+}
